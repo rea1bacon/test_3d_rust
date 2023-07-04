b776a59c06a7adf3e01b22e4641a05998c38c289
rea1bacon <victor.lefevre2004@gmail.com>
2023-07-04 16:46:17 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+/*
+ File: lib.rs
+ Created Date: 04 Jul 2023
+ Author: realbacon
+ -----
+ Last Modified: 4/07/2023 04:42:24
+ Modified By: realbacon
+ -----
+ License  : MIT
+ -----
+*/
+
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        _ => {}
+    });
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+/*
+ File: main.rs
+ Created Date: 04 Jul 2023
+ Author: realbacon
+ -----
+ Last Modified: 4/07/2023 04:42:36
+ Modified By: realbacon
+ -----
+ License  : MIT
+ -----
+*/
+
+use test_3d::run;
+
+fn main() {
+    run();
+}
